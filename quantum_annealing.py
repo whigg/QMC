829bf8f90e0caca5b764973fb904f2b00d0e745f
Yong-Gwi Cho <yonggwi.cho@gmail.com>
2018-12-11 14:51:36 +0900

    finished making a class.

--- quantum_annealing.py
+++ quantum_annealing.py
@@ -12,137 +12,123 @@ import random
 import matplotlib.pyplot as plt
 import argparse
 
-# input paramater
-#TROTTER_DIM = int(input("Trotter dimension: "))
-TROTTER_DIM = int(10)
-#ANN_PARA =  float(input("initial annealing parameter: "))
-ANN_PARA =  float(1.0)
-#ANN_STEP = int(input("Annealing Step: "))
-ANN_STEP = int(330)
-#MC_STEP = int(input("MC step: "))
-MC_STEP = int(13320)
-#BETA = float(input("inverse Temperature: "))
-BETA = float(37)
-REDUC_PARA = 0.99
-
-#FILE_NAME = 'FILE_NAME'
-#FILE_NAME =  "dj38.tsp"
-
 def distance(r1, r2):
     return math.sqrt((r1[1]-r2[1])**2 + (r1[0]-r2[0])**2)
 
-def QMC_move(config, ann_para):
-    c = np.random.randint(0,TROTTER_DIM)
-    a_ = range(1,TOTAL_TIME)
-    a = np.random.choice(a_)
-    a_.remove(a)
-    b = np.random.choice(a_)
+class QMC :
+    def __init__(self,n,p,m,l,b,r):
+        self.TROTTER_DIM = n
+        self.ANN_PARA = p
+        self.ANN_STEP = m
+        self.MC_STEP = l
+        self.BETA = b
+        self.reduc_para = r
+        self.POINT = list()
+        self.NCITY = 0
+        self.TOTAL_TIME = 0
 
-    p = config[c][a].index(1)
-    q = config[c][b].index(1)
+    def read(self,file):
+        f = open(os.path.dirname(os.path.abspath(file))+"/"+file).read().split("\n")
 
-    delta_cost = delta_costc = delta_costq_1 = delta_costq_2 = delta_costq_3 = delta_costq_4 = 0
+        for i in f:
+            self.POINT.append(i.split(" "))
 
-    for j in range(NCITY):
-        l_p_j = distance(POINT[p], POINT[j])/max_distance
-        l_q_j = distance(POINT[q], POINT[j])/max_distance
-        delta_costc += 2*(-l_p_j*config[c][a][p] - l_q_j*config[c][a][q])*(config[c][a-1][j]+config[c][(a+1)%TOTAL_TIME][j])+2*(-l_p_j*config[c][b][p] - l_q_j*config[c][b][q])*(config[c][b-1][j]+config[c][(b+1)%TOTAL_TIME][j])
+        self.NCITY = len(self.POINT)
+        self.TOTAL_TIME = self.NCITY
+        for i in range(self.NCITY):
+            self.POINT[i].remove(self.POINT[i][0])
+        for i in range(self.NCITY):
+            for j in range(2):
+                 self.POINT[i][j] = float(self.POINT[i][j])
 
-    para = (1/BETA)*math.log(math.cosh(BETA*ann_para/TROTTER_DIM)/math.sinh(BETA*ann_para/TROTTER_DIM))
-    delta_costq_1 = config[c][a][p]*(config[(c-1)%TROTTER_DIM][a][p]+config[(c+1)%TROTTER_DIM][a][p])
-    delta_costq_2 = config[c][a][q]*(config[(c-1)%TROTTER_DIM][a][q]+config[(c+1)%TROTTER_DIM][a][q])
-    delta_costq_3 = config[c][b][p]*(config[(c-1)%TROTTER_DIM][b][p]+config[(c+1)%TROTTER_DIM][b][p])
-    delta_costq_4 = config[c][b][q]*(config[(c-1)%TROTTER_DIM][b][q]+config[(c+1)%TROTTER_DIM][b][q])
+    # spin configurations at a time and some trotter dimension
+    def spin_config_at_a_time_in_a_TROTTER_DIM(self,tag):
+        config = list(-np.ones(self.NCITY, dtype = np.int))
+        config[tag] = 1
+        return config
 
-    delta_cost = delta_costc/TROTTER_DIM+para*(delta_costq_1+delta_costq_2+delta_costq_3+delta_costq_4)
-    
-    # HMC ??
-    if delta_cost <= 0:
-        config[c][a][p]*=-1
-        config[c][a][q]*=-1
-        config[c][b][p]*=-1
-        config[c][b][q]*=-1
-    elif np.random.random() < np.exp(-BETA*delta_cost):
-        config[c][a][p]*=-1
-        config[c][a][q]*=-1
-        config[c][b][p]*=-1
-        config[c][b][q]*=-1
-
-    return config
-
-    class QMC :
-        def __init__(self,n,p,m,l,b,r):
-            self.TROTTER_DIM = n
-            self.ANN_PARA = p
-            self.ANN_STEP = m
-            self.MC_STEP = l
-            self.BETA = b
-            self.reduc_para = r
-            self.POINT = list()
-
-        def read(self,file):
-            f = open(os.path.dirname(os.path.abspath(file))+"/"+file.read().split("\n")
-
-            for i in f:
-                self.POINT.append(i.split(" "))
-
-            NCITY = len(self.POINT)
-            TOTAL_TIME = NCITY
-            for i in range(NCITY):
-                self.POINT[i].remove(self.POINT[i][0])
-            for i in range(NCITY):
-                for j in range(2):
-                    self.POINT[i][j] = float(self.POINT[i][j])
-
-            def getSpinConfig(self):
-
-                def spin_config_at_a_time_in_a_TROTTER_DIM(tag):
-                     config = list(-np.ones(NCITY, dtype = np.int))
-                     config[tag] = 1
-                return config
-                     
-            def spin_config_in_a_TROTTER_DIM(tag):
-                spin = []
-                spin.append(config_at_init_time)
-                for i in xrange(TOTAL_TIME-1):
-                     spin.append(list(spin_config_at_a_time_in_a_TROTTER_DIM(tag[i])))
-            return spin
-
-            spin = []
-            for i in xrange(TROTTER_DIM):
-                tag = np.arange(1,NCITY)
-                np.random.shuffle(tag)
-            spin.append(spin_config_in_a_TROTTER_DIM(tag)) 
-
+    # sping configurations at some trotter dimension
+    def spin_config_in_a_TROTTER_DIM(self,tag):
+        spin = list()
+        spin.append(config_at_init_time)
+        for i in xrange(self.TOTAL_TIME-1):
+            spin.append(list(self.spin_config_at_a_time_in_a_TROTTER_DIM(tag[i])))
         return spin
 
-    def getBestRoute(config):   
-     length = []
-    for i in xrange(TROTTER_DIM):
-        route = []
-        for j in xrange(TOTAL_TIME):
-            route.append(config[i][j].index(1))
-        length.append(getTotaldistance(route))
+    # whole spin configurations
+    def getSpinConfig(self):
+        spin = list()
+        for i in xrange(self.TROTTER_DIM):
+            tag = np.arange(1,self.NCITY)
+            np.random.shuffle(tag)
+            spin.append(self.spin_config_in_a_TROTTER_DIM(tag))
+        return spin
 
-     min_Tro_dim = np.argmin(length)
-     Best_Route = []
-     for i in xrange(TOTAL_TIME):
-         Best_Route.append(config[min_Tro_dim][i].index(1))
-     return Best_Route
+    def getBestRoute(self,config):   
+        length = list()
+        for i in xrange(self.TROTTER_DIM):
+            route = list()
+            for j in xrange(self.TOTAL_TIME):
+                 route.append(config[i][j].index(1))
+            length.append(self.getTotaldistance(route))
 
+        min_Tro_dim = np.argmin(length)
+        Best_Route = list()
+        for i in xrange(self.TOTAL_TIME):
+            Best_Route.append(config[min_Tro_dim][i].index(1))
+        return Best_Route
 
-                     def getTotaldistance(route):
-                     Total_distance = 0
-                     for i in xrange(TOTAL_TIME):
-                     Total_distance += distance(POINT[route[i]],POINT[route[(i+1)%TOTAL_TIME]])/max_distance
-                     return Total_distance
+    def getTotaldistance(self,route):
+        Total_distance = 0
+        for i in xrange(self.TOTAL_TIME):
+            Total_distance += distance(self.POINT[route[i]],self.POINT[route[(i+1)%self.TOTAL_TIME]])/max_distance
+        return Total_distance
 
+    def getRealTotaldistance(self,route):
+        Total_distance = 0
+        for i in xrange(self.TOTAL_TIME):
+            Total_distance += distance(self.POINT[route[i]], self.POINT[route[(i+1)%self.TOTAL_TIME]])
+        return Total_distance
 
-                     def getRealTotaldistance(route):
-                     Total_distance = 0
-                     for i in xrange(TOTAL_TIME):
-                     Total_distance += distance(POINT[route[i]], POINT[route[(i+1)%TOTAL_TIME]])
-                     return Total_distance
+    def move(self,config):
+        c = np.random.randint(0,self.TROTTER_DIM)
+        a_ = range(1,self.TOTAL_TIME)
+        a = np.random.choice(a_)
+        a_.remove(a)
+        b = np.random.choice(a_)
+            
+        p = config[c][a].index(1)
+        q = config[c][b].index(1)
+
+        delta_cost = delta_costc = delta_costq_1 = delta_costq_2 = delta_costq_3 = delta_costq_4 = 0
+                
+        for j in range(self.NCITY):
+            l_p_j = distance(self.POINT[p], self.POINT[j])/max_distance
+            l_q_j = distance(self.POINT[q], self.POINT[j])/max_distance
+            delta_costc += 2*(-l_p_j*config[c][a][p] - l_q_j*config[c][a][q])*(config[c][a-1][j]+config[c][(a+1)%self.TOTAL_TIME][j]) \
+                 +2*(-l_p_j*config[c][b][p] - l_q_j*config[c][b][q])*(config[c][b-1][j]+config[c][(b+1)%self.TOTAL_TIME][j])
+
+        para = (1/self.BETA)*math.log(math.cosh(self.BETA*self.ANN_PARA/self.TROTTER_DIM)/math.sinh(self.BETA*self.ANN_PARA/self.TROTTER_DIM))
+        delta_costq_1 = config[c][a][p]*(config[(c-1)%self.TROTTER_DIM][a][p]+config[(c+1)%self.TROTTER_DIM][a][p])
+        delta_costq_2 = config[c][a][q]*(config[(c-1)%self.TROTTER_DIM][a][q]+config[(c+1)%self.TROTTER_DIM][a][q])
+        delta_costq_3 = config[c][b][p]*(config[(c-1)%self.TROTTER_DIM][b][p]+config[(c+1)%self.TROTTER_DIM][b][p])
+        delta_costq_4 = config[c][b][q]*(config[(c-1)%self.TROTTER_DIM][b][q]+config[(c+1)%self.TROTTER_DIM][b][q])
+
+        delta_cost = delta_costc/self.TROTTER_DIM+para*(delta_costq_1+delta_costq_2+delta_costq_3+delta_costq_4)
+                     
+        # HMC ??
+        if delta_cost <= 0:
+            config[c][a][p]*=-1
+            config[c][a][q]*=-1
+            config[c][b][p]*=-1
+            config[c][b][q]*=-1
+        elif np.random.random() < np.exp(-self.BETA*delta_cost):
+            config[c][a][p]*=-1
+            config[c][a][q]*=-1
+            config[c][b][p]*=-1
+            config[c][b][q]*=-1
+                     
+        return config
                      
 #QMC simulation
 if __name__ == '__main__':
@@ -156,34 +142,37 @@ if __name__ == '__main__':
     parser.add_argument("--beta",type=float,default=float(37))
     parser.add_argument("--reduc_para",type=float,default=0.99)
     args = parser.parse_args()
+
+    # prepare annealer
+    anneal = QMC(args.trotter_dim,args.ann_para,args.ann_step,args.mc_step,args.beta,args.reduc_para)
+    anneal.read(args.file)
     
     max_distance = 0
-    for i in range(NCITY):
-        for j in range(NCITY):
-            if max_distance <= distance(POINT[i], POINT[j]):
-                max_distance = distance(POINT[i], POINT[j])
+    for i in range(anneal.NCITY):
+        for j in range(anneal.NCITY):
+            if max_distance <= distance(anneal.POINT[i], anneal.POINT[j]):
+                max_distance = distance(anneal.POINT[i], anneal.POINT[j])
 
-
-    config_at_init_time = list(-np.ones(NCITY,dtype=np.int))
+    config_at_init_time = list(-np.ones(anneal.NCITY,dtype=np.int))
     config_at_init_time[0] = 1
 
     print "start..."
     t0 = time.clock()
 
     np.random.seed(100)
-    spin = getSpinConfig()
-    LengthList = []
-    for t in range(ANN_STEP):
-        for i in range(MC_STEP):
-            con = QMC_move(spin, ANN_PARA)
-            rou = getBestRoute(con)
-            length = getRealTotaldistance(rou)
+    spin = anneal.getSpinConfig()
+    LengthList = list()
+    for t in range(anneal.ANN_STEP):
+        for i in range(anneal.MC_STEP):
+            con = anneal.move(spin)
+            rou = anneal.getBestRoute(con)
+            length = anneal.getRealTotaldistance(rou)
         LengthList.append(length)
-        print "Step:{}, Annealing Parameter:{}, length:{}".format(t+1,ANN_PARA, length)
-        ANN_PARA *= REDUC_PARA
+        print "Step:{}, Annealing Parameter:{}, length:{}".format(t+1,anneal.ANN_PARA, length)
+        anneal.ANN_PARA *= anneal.reduc_para
 
-    Route = getBestRoute(spin)
-    Total_Length = getRealTotaldistance(Route)
+    Route = anneal.getBestRoute(spin)
+    Total_Length = anneal.getRealTotaldistance(Route)
     elapsed_time = time.clock()-t0
 
     print "shortest path is {}".format(Route)
