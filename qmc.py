f0b4776c30dbf4fd2a9bc153fba1f860f2b00c97
楊錬友 <yangryonu@s10252.korea-u.ac.jp>
2019-12-07 12:13:32 +0900

    modified qmc.py

--- qmc.py
+++ qmc.py
@@ -1,13 +1,14 @@
 '''
-library for simulation of quantum annealing
+library for quantum annealing
 
-(C) 2017 Yong-Gwi Cho
+(C) Yong-Gwi Cho, Sony LSI Design Inc.
 '''
 import random
 import numpy as np
 import os
 import math
 from datetime import datetime
+
 
 def distance(r1, r2):
     return math.sqrt((r1[1]-r2[1])**2 + (r1[0]-r2[0])**2)
@@ -100,9 +101,8 @@ class QMC :
             Total_distance += distance(self.POINT[route[i]], self.POINT[route[(i+1)%self.TOTAL_TIME]])
         return Total_distance
 
-    def Hamiltonian(self):
-        "write Hamiltonian"
-        
+
+
     def move(self,conf):
         c = np.random.randint(0,self.TROTTER_DIM)
         a_ = range(1,self.TOTAL_TIME)
@@ -113,30 +113,77 @@ class QMC :
         p = conf[c][a].index(1)
         q = conf[c][b].index(1)
 
-        delta_E = delta_costc = delta_costq_1 = delta_costq_2 = delta_costq_3 = delta_costq_4 = 0
+        delta_cost = delta_costc = delta_costq_1 = delta_costq_2 = delta_costq_3 = delta_costq_4 = 0.0
 
         for j in range(self.NCITY):
             l_p_j = distance(self.POINT[p], self.POINT[j])/self.max_distance
             l_q_j = distance(self.POINT[q], self.POINT[j])/self.max_distance
-            delta_Ec += 2*(-l_p_j*conf[c][a][p] - l_q_j*conf[c][a][q])*(conf[c][a-1][j]+conf[c][(a+1)%self.TOTAL_TIME][j]) \
+            delta_costc += 2*(-l_p_j*conf[c][a][p] - l_q_j*conf[c][a][q])*(conf[c][a-1][j]+conf[c][(a+1)%self.TOTAL_TIME][j]) \
                            +2*(-l_p_j*conf[c][b][p] - l_q_j*conf[c][b][q])*(conf[c][b-1][j]+conf[c][(b+1)%self.TOTAL_TIME][j])
 
-        para = (1/self.BETA)*\
-            math.log(math.cosh(self.BETA*self.ANN_PARA/self.TROTTER_DIM)/math.sinh(self.BETA*self.ANN_PARA/self.TROTTER_DIM))
+        para = (1/self.BETA)*math.log(math.cosh(self.BETA*self.ANN_PARA/self.TROTTER_DIM)/math.sinh(self.BETA*self.ANN_PARA/self.TROTTER_DIM))
         delta_costq_1 = conf[c][a][p]*(conf[(c-1)%self.TROTTER_DIM][a][p]+conf[(c+1)%self.TROTTER_DIM][a][p])
         delta_costq_2 = conf[c][a][q]*(conf[(c-1)%self.TROTTER_DIM][a][q]+conf[(c+1)%self.TROTTER_DIM][a][q])
         delta_costq_3 = conf[c][b][p]*(conf[(c-1)%self.TROTTER_DIM][b][p]+conf[(c+1)%self.TROTTER_DIM][b][p])
         delta_costq_4 = conf[c][b][q]*(conf[(c-1)%self.TROTTER_DIM][b][q]+conf[(c+1)%self.TROTTER_DIM][b][q])
 
-        delta_E = delta_costc/self.TROTTER_DIM+para*(delta_costq_1+delta_costq_2+delta_costq_3+delta_costq_4)
+        delta_cost = delta_costc/self.TROTTER_DIM+para*(delta_costq_1+delta_costq_2+delta_costq_3+delta_costq_4)
 
-        # accept spin flipping by min(1,exp(-beta*delta_cost))
+        # accep spin flippign by min(1,exp(-beta*delta_cost))
         if delta_cost <= 0:
             conf[c][a][p]*=-1
             conf[c][a][q]*=-1
             conf[c][b][p]*=-1
             conf[c][b][q]*=-1
-        elif np.random.random() < np.exp(-self.BETA*delta_E):
+        elif np.random.random() < np.exp(-self.BETA*delta_cost):
+            conf[c][a][p]*=-1
+            conf[c][a][q]*=-1
+            conf[c][b][p]*=-1
+            conf[c][b][q]*=-1
+
+        return conf
+
+    def move_imp(self,conf):
+        c = np.random.randint(0,self.TROTTER_DIM)
+        a_ = range(1,self.TOTAL_TIME)
+        a = np.random.choice(a_)
+        a_.remove(a)
+        b = np.random.choice(a_)
+
+        p = conf[c][a].index(1)
+        q = conf[c][b].index(1)
+
+        delta_cost = delta_costc = delta_costq_1 = delta_costq_2 = delta_costq_3 = delta_costq_4 = 0.0
+
+        for j in range(self.NCITY):
+            d_pj = distance(self.POINT[p], self.POINT[j])
+            d_qj = distance(self.POINT[q], self.POINT[j])
+            if a+1 >= self.NCITY:
+                delta_costc += 2*(conf[c][a-1][j] + conf[c][1][j] - conf[c][b-1][j] - conf[c][b+1][j])(d_qj - d_pj)
+            elif a-1 < 1:
+                delta_costc += 2*(conf[c][self.NCITY][j] + conf[c][a+1][j] - conf[c][b-1][j] - conf[c][b+1][j])(d_qj - d_pj)
+            elif b+1 >= self.NCITY:
+                delta_costc += 2*(conf[c][a-1][j] + conf[c][a+1][j] - conf[c][b-1][j] - conf[c][1][j])(d_qj - d_pj)
+            elif b-1 < 1:
+                delta_costc += 2*(conf[c][a-1][j] + conf[c][a+1][j] - conf[c][self.NCITY][j] - conf[c][b+1][j])(d_qj - d_pj)
+            else :
+                delta_costc += 2*(conf[c][a-1][j] + conf[c][a+1][j] - conf[c][b-1][j] - conf[c][b+1][j])(d_qj - d_pj)
+
+        para = (1/self.BETA)*math.log(math.cosh(self.BETA*self.ANN_PARA/self.TROTTER_DIM)/math.sinh(self.BETA*self.ANN_PARA/self.TROTTER_DIM))
+        delta_costq_1 = conf[c][a][p]*(conf[c-1][a][p]+conf[c+1][a][p])
+        delta_costq_2 = conf[c][a][q]*(conf[c-1][a][q]+conf[c+1][a][q])
+        delta_costq_3 = conf[c][b][p]*(conf[c-1][b][p]+conf[c+1][b][p])
+        delta_costq_4 = conf[c][b][q]*(conf[c-1][b][q]+conf[c+1][b][q])
+
+        delta_cost = delta_costc/self.TROTTER_DIM + para*(delta_costq_1 + delta_costq_2 + delta_costq_3 + delta_costq_4)
+
+        # accep spin flippign by min(1,exp(-beta*delta_cost))
+        if delta_cost <= 0:
+            conf[c][a][p]*=-1
+            conf[c][a][q]*=-1
+            conf[c][b][p]*=-1
+            conf[c][b][q]*=-1
+        elif np.random.random() < np.exp(-self.BETA*delta_cost):
             conf[c][a][p]*=-1
             conf[c][a][q]*=-1
             conf[c][b][p]*=-1
